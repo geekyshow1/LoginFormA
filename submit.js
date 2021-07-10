d937a3c172e78e950d18989fb78588ddf31504af
Sandeep <sandeeep.github@gmail.com>
2021-07-10 09:47:33 +0530

    change - added js script to give alert on submit

---
+++ submit.js
@@ -0,0 +1,9 @@
+const email = document.getElementsByClassName('.email');
+const password = document.getElementsByClassName('.password');
+const button = document.getElementById('login-button');
+
+
+button.addEventListener('click', () => {
+        alert('You have been logged in');
+    
+})
